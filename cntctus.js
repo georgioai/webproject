a2f882d6aac3b67d1770f7378b5c3d18bad66287
georgio abou ibrahim <georgio.abouibrahim@net.usek.edu.lb>
2025-05-05 01:50:37 +0300

    UPDATED

---
+++ cntctus.js
@@ -0,0 +1,122 @@
+const $ = function(id)
+{
+    return document.getElementById(id);
+}
+
+const submit = function(event)
+{
+    const firstname = $("fnfield").value;
+    const lastname= $("lnfield").value;
+    const emailv = $("emailfield").value;
+    const phnn = $("phnfield").value;
+    const msg = $("txtarfield").value;
+    
+    let flag=true;
+
+    if(firstname=="")
+    {
+        $("errorfn").textContent="\nERROR FIRSTNAME REQUIRED!";
+        errorfn.style.fontSize="12px";
+        errorfn.style.fontFamily="monospace";
+        errorfn.style.color="red";
+        fnfield.style.border="2px solid red";
+        flag=false;
+    }else if(!isNaN(parseInt(firstname))){
+        $("errorfn").textContent="INPUT NOT VALID!";
+        errorfn.style.fontSize="12px";
+        errorfn.style.fontFamily="monospace";
+        errorfn.style.color="red";
+        fnfield.style.border="2px solid red";
+        flag=false;
+    }
+
+    if(lastname=="")
+        {
+            $("errorln").textContent="ERROR LASTNAME REQUIRED!";
+            errorln.style.fontSize="12px";
+            errorln.style.fontFamily="monospace";
+            errorln.style.color="red";
+            lnfield.style.border="2px solid red";
+            flag=false;
+        }else if(!isNaN(parseInt(lastname))){
+            $("errorln").textContent="INPUT NOT VALID!";
+            errorln.style.fontSize="12px";
+            errorln.style.fontFamily="monospace";
+            errorln.style.color="red";
+            lnfield.style.border="2px solid red";
+            flag=false;
+        }
+    
+        if(emailv=="")
+            {
+                $("errorem").textContent="ERROR EMAIL REQUIRED!";
+                errorem.style.fontSize="12px";
+                errorem.style.fontFamily="monospace";
+                errorem.style.color="red";
+                emailfield.style.border="2px solid red";
+                flag=false;
+            }
+        
+        if(!phnn=="" && isNaN(parseInt(phnn)))
+        {
+            $("errorph").textContent="ONLY INTEGERS ARE VALID!";
+            errorph.style.fontSize="12px";
+            errorph.style.fontFamily="monospace";
+            errorph.style.color="red";
+            phnfield.style.border="2px solid red";
+            flag=false;
+        }
+
+        if(msg=="")
+            {
+                $("errorms").textContent="ERROR MESSAGE REQUIRED!";
+                errorms.style.fontSize="12px";
+                errorms.style.fontFamily="monospace";
+                errorms.style.color="red";
+                txtarfield.style.border="2px solid red";
+                flag=false;
+            }
+        
+            if(flag){
+                $("greeting").textContent="THANK YOU! AN AGENT WILL BE WITH YOU SOON...";
+                greeting.style.color="white";
+            }
+            reset();
+
+
+}
+const reset = function(){
+    fnfield.value="";
+    lnfield.value="";
+    emailfield.value="";
+    phnfield.value="";
+    txtarfield.value="";
+
+    $("errorfn").textContent="*";
+    errorfn.style.color="black";
+    $("errorln").textContent="*";
+    errorln.style.color="black";
+    $("errorem").textContent="*";
+    errorem.style.color="black";
+    $("errorph").textContent="";
+    errorph.style.color="black";
+    $("errorms").textContent="*";
+    errorms.style.color="black";
+
+    fnfield.style.border="2px solid black";
+    lnfield.style.border="2px solid black";
+    emailfield.style.border="2px solid black";
+    phnfield.style.border="2px solid black";
+    txtarfield.style.border="2px solid black";
+
+
+
+
+
+}
+window.onload = function(){
+    $("btn1").onclick = submit;
+    $("btn2").onclick=reset;
+    $("fnfield").focus();
+
+}
