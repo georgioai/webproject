64e3d88a4d334984dd6c0aad22c168639bc97398
georgio abou ibrahim <georgio.abouibrahim@net.usek.edu.lb>
2025-05-06 17:27:45 +0300

    n

--- contactusAction.php
+++ contactusAction.php
@@ -3,11 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylecs02.css">
     <title>GREETING MESSAGE</title>
 </head>
+<style>
+    h1{
+        color:white;
+    }
+</style>
 <body>
     <?php 
     echo "<h1>THANK YOU FOR SUBMITTING</h1>";
+    echo "First name:" . htmlspecialchars($_POST['fnfield']);
     ?>
     
 </body>
